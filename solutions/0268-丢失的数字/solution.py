f44ad71c50a60ca40b0852d0f4539ca40ff6dfaa
MysteryMulberry <1738718993@qq.com>
2026-05-22 14:58:55 +0800

    feat: 添加LeetCode268 丢失的数字题解 (数学)

---
+++ solutions/0268-丢失的数字/solution.py
@@ -0,0 +1,23 @@
+"""
+LeetCode 268: 丢失的数字
+难度: Easy
+方法: 数学
+时间复杂度: O(n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return n * (n + 1) // 2 - sum(nums)
