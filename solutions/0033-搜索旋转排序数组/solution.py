d9ff4f2d4f8ce61e90017b8bfef41ce0825429e6
MysteryMulberry <1738718993@qq.com>
2026-05-22 13:06:45 +0800

    feat: 添加LeetCode33 搜索旋转排序数组题解 (二分查找)

---
+++ solutions/0033-搜索旋转排序数组/solution.py
@@ -0,0 +1,37 @@
+"""
+LeetCode 33: 搜索旋转排序数组
+难度: Medium
+方法: 二分查找
+时间复杂度: O(log n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
