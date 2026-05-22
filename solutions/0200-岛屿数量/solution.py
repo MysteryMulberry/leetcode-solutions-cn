d05a1195af06d04c360be4cc0510d0594d6c3ebc
MysteryMulberry <1738718993@qq.com>
2026-05-22 13:07:23 +0800

    feat: 添加LeetCode200 岛屿数量题解 (DFS/BFS)

---
+++ solutions/0200-岛屿数量/solution.py
@@ -0,0 +1,38 @@
+"""
+LeetCode 200: 岛屿数量
+难度: Medium
+方法: DFS/BFS
+时间复杂度: O(mn)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
