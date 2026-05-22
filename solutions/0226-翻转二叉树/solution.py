5eb8c89e157fc2667f8f4d5c30d158473c18b02f
MysteryMulberry <1738718993@qq.com>
2026-05-22 13:07:27 +0800

    feat: 添加LeetCode226 翻转二叉树题解 (递归)

---
+++ solutions/0226-翻转二叉树/solution.py
@@ -0,0 +1,25 @@
+"""
+LeetCode 226: 翻转二叉树
+难度: Easy
+方法: 递归
+时间复杂度: O(n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
