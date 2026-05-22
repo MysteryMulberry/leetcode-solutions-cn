3e7c7466b0928d4215d3e5b0ebd4d74c682e0863
MysteryMulberry <1738718993@qq.com>
2026-05-22 14:58:34 +0800

    feat: 添加LeetCode102 二叉树的层序遍历题解 (BFS)

---
+++ solutions/0102-二叉树的层序遍历/solution.py
@@ -0,0 +1,33 @@
+"""
+LeetCode 102: 二叉树的层序遍历
+难度: Medium
+方法: BFS
+时间复杂度: O(n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+        result = []
+        queue = [root]
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            result.append(level)
+        return result
