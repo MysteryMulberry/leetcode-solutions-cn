c8fe1bace531f17d03d90b1c8acfa781e4bf5392
MysteryMulberry <1738718993@qq.com>
2026-05-22 13:07:38 +0800

    feat: 添加LeetCode338 比特位计数题解 (动态规划)

---
+++ solutions/0338-比特位计数/solution.py
@@ -0,0 +1,25 @@
+"""
+LeetCode 338: 比特位计数
+难度: Easy
+方法: 动态规划
+时间复杂度: O(n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
