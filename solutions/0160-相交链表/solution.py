14bf681f96ef02c1b94db9db9b71e7ece9e5e7b4
MysteryMulberry <1738718993@qq.com>
2026-05-22 14:58:41 +0800

    feat: 添加LeetCode160 相交链表题解 (双指针)

---
+++ solutions/0160-相交链表/solution.py
@@ -0,0 +1,26 @@
+"""
+LeetCode 160: 相交链表
+难度: Easy
+方法: 双指针
+时间复杂度: O(m+n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        a, b = headA, headB
+        while a != b:
+            a = a.next if a else headB
+            b = b.next if b else headA
+        return a
