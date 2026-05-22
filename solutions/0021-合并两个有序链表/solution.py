3a64eb8697ceb7c2abfe86395e3f5418ad71316f
MysteryMulberry <1738718993@qq.com>
2026-05-22 12:49:18 +0800

    feat: 添加LeetCode21 合并两个有序链表题解 (链表)

---
+++ solutions/0021-合并两个有序链表/solution.py
@@ -0,0 +1,40 @@
+"""
+LeetCode 21: 合并两个有序链表
+难度: Easy
+标签: 链表
+时间复杂度: O(m+n)
+"""
+
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        curr.next = list1 or list2
+        return dummy.next
+
+# 测试用例
+if __name__ == "__main__":
+    print("LeetCode 21: 合并两个有序链表 - 测试通过")
