538e5706da5a3fd92d56768becec7d376eb9d41c
MysteryMulberry <1738718993@qq.com>
2026-05-22 13:07:08 +0800

    feat: 添加LeetCode104 二叉树的最大深度题解 (DFS递归)

---
+++ solutions/0104-二叉树的最大深度/solution.py
@@ -0,0 +1,24 @@
+"""
+LeetCode 104: 二叉树的最大深度
+难度: Easy
+方法: DFS递归
+时间复杂度: O(n)
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
