2958f7ea998ddba6f91784a1fb5511422cbf253a
MysteryMulberry <1738718993@qq.com>
2026-05-22 14:59:09 +0800

    feat: 添加LeetCode617 合并二叉树题解 (递归)

---
+++ solutions/0617-合并二叉树/solution.py
@@ -0,0 +1,27 @@
+"""
+LeetCode 617: 合并二叉树
+难度: Easy
+方法: 递归
+时间复杂度: O(min(m,n))
+"""
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1: return root2
+        if not root2: return root1
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
